6e2c8ee0f16ff93b60abd0def55c6a07978a3b6a
kingporras <victorfifa723@gmail.com>
2025-06-15 05:06:26 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXDsSktfsiFVQlM4hYFMnNVAipUPyV8rw",
+  authDomain: "idvapp-807bd.firebaseapp.com",
+  projectId: "idvapp-807bd",
+  storageBucket: "idvapp-807bd.firebasestorage.app",
+  messagingSenderId: "1081860764292",
+  appId: "1:1081860764292:web:4d4a7b4daf41259f8bc8d1",
+  measurementId: "G-5Z8KXKXWZS"
+};
